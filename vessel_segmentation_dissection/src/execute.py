00de41edc0d04bc8ebb37ab8e74d6884955c1979
weslley-ss <weslle.ws@gmail.com>
2026-02-07 04:04:04 -0300

    feat(src/execute): Add loop for first Vessmap trains on labs PC

--- vessel_segmentation_dissection/src/execute.py
+++ vessel_segmentation_dissection/src/execute.py
@@ -192,9 +192,14 @@ def main():
     base_res = [1]*5 
     res_vals = [3]
 
+    standard_config = [[(3, 3, 3, 3, 3, 3, 3, 3, 3, 3), (1, 1, 1, 1, 1, 1, 1, 1, 1, 1), (1, 1, 1, 1, 1), (2, 2)]]
     configs = generate_models_configurations(base_k, base_dil, base_res, k_vals, dil_vals, res_vals, return_base = False)
-    
-    ciclo_treino_transform(configs, seed=2, data="VessMAP", model="dic_unet", transform="skeleton", variation ="ratio_1")
+
+    for seed in [2,4,8]:
+        ciclo_treino_transform(configs, seed=seed, data="VessMAP", model="dic_unet")
+        #ciclo_treino_transform(configs, seed=seed, data="DRIVE", model="dic_unet")
+        #ciclo_treino_transform(standard_config, seed=2, data="VessMAP", model="dic_unet", transform="erosion")
+        #ciclo_treino_transform(standard_config, seed=2, data="VessMAP", model="dic_unet", transform="skeleton")
 
 
 main()
